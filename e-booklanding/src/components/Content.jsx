4154620b01c0aeee64f1597c2d3e742b7d45da60
Brian H. Castelhano <118952129+briantheengineer@users.noreply.github.com>
2025-06-09 20:39:46 -0300

    content svgs

--- e-booklanding/src/components/Content.jsx
+++ e-booklanding/src/components/Content.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 export default function Content () {
     return (
-        <div className="h-max text-center flex justify-center items-center">
+        <div className="h-screen text-center flex justify-center items-center">
             <div className="border p-5 w-full h-full">
                 <h1 className="text-3xl mb-5">Explore nossa Biblioteca</h1>
-                <div className="w-full h-full flex flex-nowrap justify-center p-5">
-                   <div className="w-4/6 border h-full rounded-2xl mx-2 overflow-hidden" 
-                   style={{ backgroundImage: "url('/svgs/blurry.svg')" }}>
+                <div className="w-full h-full flex lg:flex-nowrap sm:flex-wrap justify-center p-5">
+                   <div className="w-[30%] min-w-80 h-[95%] border rounded-2xl mx-2" 
+                   style={{ backgroundImage: "url('/svgs/low-poly-grid-haikei.svg')" }}>
                     <h1>asdasda</h1>
                    </div>
-                   <div className="w-4/6 border h-fit rounded-2xl mx-2 overflow-hidden">
-                    <img></img>
+                   <div className="w-[30%] min-w-80 border  h-[95%] rounded-2xl mx-2"
+                   style={{ backgroundImage: "url('/svgs/stacked-peaks-haikei.svg')" }}>
                    </div>
-                   <div className="w-4/6 border h-fit rounded-2xl mx-2 overflow-hidden">
-                    <img></img>
+                   <div className="min-w-80 w-[30%] border  h-[95%] rounded-2xl mx-2"
+                   style={{ backgroundImage: "url('/svgs/blob-scene-haikei.svg')" }}>
                    </div>
 
                 </div>
